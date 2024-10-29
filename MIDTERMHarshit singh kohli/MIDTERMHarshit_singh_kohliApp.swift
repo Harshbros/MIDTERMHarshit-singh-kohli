86b59cf74fa1f491a637dca1328b015b0d7f29b4
Harshbros <harshitsinghkohli@gmail.com>
2024-10-29 17:21:59 -0400

    Initial Commit

---
+++ MIDTERMHarshit singh kohli/MIDTERMHarshit_singh_kohliApp.swift
@@ -0,0 +1,17 @@
+//
+//  MIDTERMHarshit_singh_kohliApp.swift
+//  MIDTERMHarshit singh kohli
+//
+//  Created by Harshit Singh Kohli on 2024-10-29.
+//
+
+import SwiftUI
+
+@main
+struct MIDTERMHarshit_singh_kohliApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ MIDTERMHarshit singh kohliTests/MIDTERMHarshit_singh_kohliTests.swift
@@ -0,0 +1,17 @@
+//
+//  MIDTERMHarshit_singh_kohliTests.swift
+//  MIDTERMHarshit singh kohliTests
+//
+//  Created by Harshit Singh Kohli on 2024-10-29.
+//
+
+import Testing
+@testable import MIDTERMHarshit_singh_kohli
+
+struct MIDTERMHarshit_singh_kohliTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
